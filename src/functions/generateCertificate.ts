f9250d03dbdfd433720a279f090b7cb76ddac61a
lucas.lira <lucasrocha446@gmail.com>
2021-06-13 19:07:59 -0300

    feat: add certificate S3

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,30 +1,112 @@
-import { document } from '../utils/dynamodbClient'
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import fs from "fs";
+import handlebars from "handlebars"
+import dayjs from "dayjs"
+import { S3 } from "aws-sdk"
+
+import { document } from "../utils/dynamodbClient"
 
 interface ICreateCertificate {
+  id: string,
+  name: string,
+  grade: string
+}
+
+interface ITemplate {
   id: string;
   name: string;
   grade: string;
+  date: string;
+  medal: string
+}
+
+const compile = async function (data:ITemplate) {
+  const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs")
+
+  const html = fs.readFileSync(filePath, "utf-8")
+
+  return handlebars.compile(html)(data)
 }
 
 export const handle = async (event) => {
-  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  document.put({
-    TableName: 'users_certificates',
-    Item: {
-      id,
-      name,
-      grade
+  const response = await document.query({
+    TableName: "users_certificates",
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id" :id
     }
-  }).promise().then((res) => console.log(res))
+  }).promise()
+
+  const userAlreadyExists = response.Items[0]
+
+  if(!userAlreadyExists) {
+    await document.put({
+      TableName: "users_certificates",
+      Item: {
+        id,
+        name,
+        grade
+      }
+    }).promise()
+  }
+
+
+  const medalPath = path.join(process.cwd(), "src", "templates", "selo.png")
+  const medal = fs.readFileSync(medalPath, "base64")
+
+  const data:ITemplate = {
+    date: dayjs().format("DD/MM/YYYY"),
+    grade,
+    name,
+    id,
+    medal
+  }
+
+  const content = await compile(data)
+
+  const browser = await chromium.puppeteer.launch({
+    headless: true,
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath
+  })
+
+  const page = await browser.newPage()
+
+  await page.setContent(content)
+
+  const pdfPath = path.join(process.cwd(), "pdf/")
+  const pdf = await page.pdf({
+    format: "a4",
+    landscape: true,
+    path: process.env.IF_OFFLINE ? pdfPath + "certificate.pdf" : null,
+    printBackground: true,
+    preferCSSPageSize: true
+  })
+
+  await  browser.close()
+
+  const s3 = new S3()
+
+  await s3.putObject({
+    Bucket: "serverlesscertificates",
+    Key: `${id}.pdf`,
+    ACL: "public-read",
+    Body: pdf,
+    ContentType: "application/pdf"
+  }).promise()
 
   return {
     statusCode: 201,
     body: JSON.stringify({
-      message: 'Certificate created!'
+      message: "Certificate created!",
+      url: `https://serverlesscertificates.s3.sa-east-1.amazonaws.com/${id}.pdf`
     }),
     headers: {
-      'Content-type': 'application/json'
+      "Content-Type": "application/json"
     }
   }
 }
